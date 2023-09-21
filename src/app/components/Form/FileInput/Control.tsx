77ca93f90af807e61a499b49f61b1c3668a31c53
Gustavo-Jezini <Gustavojezini@gmail.com>
2023-09-21 14:14:03 -0300

    code

---
+++ src/app/components/Form/FileInput/Control.tsx
@@ -0,0 +1,30 @@
+'use client'
+import { ChangeEvent, ComponentProps } from 'react'
+import { useFileInput } from './Root'
+
+export type ControlProps = ComponentProps<'input'>
+
+export function Control({ multiple = false, ...props }: ControlProps) {
+  const { id, onFilesSelected } = useFileInput()
+
+  function handleFilesSelected(event: ChangeEvent<HTMLInputElement>) {
+    if (!event.target.files?.length) {
+      return
+    }
+
+    const files = Array.from(event.target.files)
+
+    onFilesSelected(files, multiple)
+  }
+
+  return (
+    <input
+      type="file"
+      onChange={handleFilesSelected}
+      className="sr-only px-4 py-5"
+      multiple={multiple}
+      id={id}
+      {...props}
+    />
+  )
+}
---
+++ src/app/components/Sidebar/Profile.tsx
@@ -0,0 +1,27 @@
+import { LogOut } from 'lucide-react'
+import { Button } from '../Button'
+
+export function Profile() {
+  return (
+    <div className=" flex items-center gap-3">
+      <img
+        src="https://github.com/gustavo-jezini.png"
+        className="h-10 w-10 rounded-full"
+        alt=""
+      />
+
+      <div className="flex flex-1 flex-col truncate">
+        <span className="text-sm font-semibold text-zinc-700 dark:text-zinc-100">
+          Gustavo Jezini Matos
+        </span>
+        <span className="truncate text-sm text-zinc-500 dark:text-zinc-400">
+          Gustavojezini@gmail.com
+        </span>
+      </div>
+
+      <Button type="button" variant="ghost">
+        <LogOut className="h-5 w-5 text-zinc-500" />
+      </Button>
+    </div>
+  )
+}
